2f2b45cc4ac5dd4bbe533a69f9ad1868a5e5eaa7
noahkise <noah@kisefamily.com>
2024-02-09 10:03:20 -0800

    add reusable form for machine add and edit

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+    public class MachinesController : Controller
+    {
+        private readonly FactoryContext _db;
+        public MachinesController(FactoryContext db)
+        {
+            _db = db;
+        }
+
+
+        public ActionResult Index()
+        {
+            List<Machine> model = _db.Machines.ToList();
+            return View(model);
+        }
+
+        [HttpGet("/machines/create/")]
+        public ActionResult Create()
+        {
+            Dictionary<string, object> model = new() {
+        {"Machine", new Machine()},
+        {"Engineers", _db.Engineers.ToList()},
+        {"Action", "Create"}
+      };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Machine machine, List<int> EngineerIds)
+        {
+            _db.Machines.Add(machine);
+            _db.SaveChanges();
+
+            if (EngineerIds != null && EngineerIds.Any())
+            {
+                foreach (int engineerId in EngineerIds)
+                {
+                    _db.EngineerMachines.Add(new EngineerMachine
+                    {
+                        MachineId = machine.MachineId,
+                        EngineerId = engineerId
+                    });
+                }
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
