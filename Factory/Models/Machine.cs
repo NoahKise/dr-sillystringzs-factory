487ad023b0c6ee1486317503cee21177afbf3779
noahkise <noah@kisefamily.com>
2024-02-09 13:01:56 -0800

    splash page lists all engineers and machines

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,7 +6,7 @@ namespace Factory.Models;
 public class Machine
 {
     public int MachineId { get; set; }
-    [Required(ErrorMessage = "The machine needs a name")]
+    [Required]
     public string Name { get; set; }
     public string ImageUrl { get; set; }
     public List<EngineerMachine> EngineerMachines { get; }
