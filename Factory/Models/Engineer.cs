766fc15cc5437c55c5f2e498939a92dc61232ee1
noahkise <noah@kisefamily.com>
2024-02-09 08:45:29 -0800

    add minimum project file strucure, boilerplate code

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models;
+
+public class Engineer
+{
+    public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The engineer must have a name")]
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models;
+
+public class Machine
+{
+    public int MachineId { get; set; }
+    [Required(ErrorMessage = "The machine needs a name")]
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+}
